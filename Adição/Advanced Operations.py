5ca2dc3a5ffd2c9013cf6d0e7e36438c6dcb24cc
Matheus Barros <89200586+S3EMi@users.noreply.github.com>
2021-08-19 12:35:27 -0300

    Update Advanced Operations.py

--- Adição/Advanced Operations.py
+++ Adição/Advanced Operations.py
@@ -2,9 +2,10 @@
 # Série: 2 ano EM
 # A versão mais simples pode ser encontrada aqui: https://github.com/S3EMi/Class1/blob/main/Adição/Simple%20Addition.py
 
+
 # função para "pausar"
 def pause():
-    pauseProgram = input("Pressione a tecla ENTER para continuar")
+    pauseProgram = input("\nPressione a tecla ENTER para continuar")
 
 # adicionar
 def add(x, y):
@@ -23,32 +24,39 @@ def divide(x, y):
     return x / y
 
 # escolher a operação
-print("Escolha a operação.")
+print("\nEscolha a operação.")
 print("1. Adicionar")
 print("2. Subtrair")
 print("3. Multiplicar")
 print("4. Dividir")
 
 # verifique a escolha da alternativa e faça a operação
-choice = input("Selecione a alternativa 1, 2, 3, 4: ")
+choice = input("\nSelecione a alternativa 1, 2, 3, 4: ")
 if choice in ('1', '2', '3', '4'):
+  num1 = input("\nDigite o primeiro número: ")
+  num2 = input("Digite o segundo número: ")
 
-  num1 = float(input("Digite o primeiro número: "))
-  num2 = float(input("Digite o segundo número: "))
+# verifica se o valor dado é um número ou não
+  try:
+    float(num1), float(num2)
+  except ValueError:
+    print("\nUm ou mais valores não foram identificados como números, se você quer usar decimais utilize '.' e não ','")
+    pause()
+    exit()
 
 # verifica a escolha e faz a operação
 if choice == '1':
-  print(num1, " + ", num2, " = ", add(num1, num2))
+  print(num1, " + ", num2, " = ", add(float(num1), float(num2)))
 
 
 elif choice == '2':
-  print(num1, " - ", num2, " = ", subtract(num1, num2))
+  print(num1, " - ", num2, " = ", subtract(float(num1), float(num2)))
 
 elif choice == '3':
-  print(num1, " x ", num2, " = ", multiply(num1, num2))
+  print(num1, " x ", num2, " = ", multiply(float(num1), float(num2)))
 
 elif choice == '4':
-  print(num1, " / ", num2, " = ", divide(num1, num2))
+  print(num1, " / ", num2, " = ", divide(float(num1), float(num2)))
 
 else:
     print("Escolha inválida.")
