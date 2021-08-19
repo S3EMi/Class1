1b0ff48ceb427ab13527113ba8d6afa085c98832
Matheus Barros <89200586+S3EMi@users.noreply.github.com>
2021-08-19 11:28:35 -0300

    Update Simple Addition.py

--- Adição/Simple Addition.py
+++ Adição/Simple Addition.py
@@ -1,8 +1,14 @@
+# Nome: Matheus Barros
+# Série: 1 ano EM
+
 def pause():
     pause = input("Pressione ENTER para continuar")
 
 n1 = (input("Primeiro Número: "))
 n2 = (input("Segundo Número: "))
+
+# Dar um erro bem mais simplificado se o valor dado não for um número
+# Verifica se os números são inteiros ou decimais, se não, retorna com um erro e fecha o programa
 try:
     int(n1), int(n2)
 except ValueError:
