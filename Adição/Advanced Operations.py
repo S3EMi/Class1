c3651b97968f60e40d8d9bf83f697ef9aad7c7ea
Matheus Barros <89200586+S3EMi@users.noreply.github.com>
2021-08-20 09:51:39 -0300

    Update Advanced Operations.py

--- Adição/Advanced Operations.py
+++ Adição/Advanced Operations.py
@@ -7,88 +7,94 @@
 
 import math as mt
 
-def pause():
-    pauseProgram = input("\nPressione a tecla ENTER para continuar")
+def restart():
+  def gotor():
+    if pause() == "":
+      restart()
+    else:
+      restart()
 
-# adicionar
-def add(x, y):
-    return x + y
+  def pause():
+      input("\nPressione a tecla ENTER para continuar...")
 
-# subtrair
-def subtract(x, y):
-    return x - y
+  # adicionar
+  def add(x, y):
+      return x + y
 
-# multiplicar
-def multiply(x, y):
-    return x * y
+  # subtrair
+  def subtract(x, y):
+      return x - y
 
-# dividir
-def divide(x, y):
-    return x / y
+  # multiplicar
+  def multiply(x, y):
+      return x * y
 
-# escolher a operação
-print("\nEscolha a operação.")
-print("1. Adicionar")
-print("2. Subtrair")
-print("3. Multiplicar")
-print("4. Dividir")
-print("5. Calcular Bhaskara")
+  # dividir
+  def divide(x, y):
+      return x / y
 
-# verifique a escolha da alternativa e faça a operação
-choice = input("\nSelecione a alternativa 1, 2, 3, 4, 5: ")
-if choice in ('1', '2', '3', '4'):
-  num1 = input("\nDigite o primeiro número: ")
-  num2 = input("Digite o segundo número: ")
+  # escolher a operação
+  print("\n\nEscolha a operação.")
+  print("1. Adicionar")
+  print("2. Subtrair")
+  print("3. Multiplicar")
+  print("4. Dividir")
+  print("5. Calcular Bhaskara")
 
-# verifica se o valor dado é um número ou não
-  try:
-    float(num1), float(num2)
-  except ValueError:
-    print("\nUm ou mais valores não foram identificados como números, se você quer usar decimais utilize '.' e não ','")
-    pause()
-    exit()
+  # verifique a escolha da alternativa e faça a operação
+  choice = input("\nSelecione a alternativa 1, 2, 3, 4, 5: ")
+  if choice in ('1', '2', '3', '4'):
+    num1 = input("\nDigite o primeiro número: ")
+    num2 = input("Digite o segundo número: ")
 
-# verifica a escolha e faz a operação
-if choice == '1':
-  print(num1, " + ", num2, " = ", add(float(num1), float(num2)))
+  # verifica se o valor dado é um número ou não
+    try:
+      float(num1), float(num2)
+    except ValueError:
+      print("\nUm ou mais valores não foram identificados como números, se você quer usar decimais utilize '.' e não ','")
+      gotor()
+
+  # verifica a escolha e faz a operação
+  if choice == '1':
+    print(num1, " + ", num2, " = ", add(float(num1), float(num2)))
 
 
-elif choice == '2':
-  print(num1, " - ", num2, " = ", subtract(float(num1), float(num2)))
+  elif choice == '2':
+    print(num1, " - ", num2, " = ", subtract(float(num1), float(num2)))
 
-elif choice == '3':
-  print(num1, " x ", num2, " = ", multiply(float(num1), float(num2)))
+  elif choice == '3':
+    print(num1, " x ", num2, " = ", multiply(float(num1), float(num2)))
 
-elif choice == '4':
-  print(num1, " / ", num2, " = ", divide(float(num1), float(num2)))
+  elif choice == '4':
+    print(num1, " / ", num2, " = ", divide(float(num1), float(num2)))
 
-elif choice == '5':
+  elif choice == '5':
 
-  a = float(input("Primeiro número (a): "))
-  b = float(input("Segundo número (b): "))
-  c = float(input("Terceiro número (c): "))
+    a = input("Primeiro número (a): ")
+    b = input("Segundo número (b): ")
+    c = input("Terceiro número (c): ")
+    try:
+        float(a), float(b), float(c)
+    except ValueError:
+        print("Um ou mais valores dados não foram identificados")
+        gotor()
+    a = float(a)
+    b = float(b)
+    c = float (c)
+    disc = (b**2 - 4*a*c)
+    try:
+        res1 = (-b - mt.sqrt(disc))/(2*a)
+        res2 = (-b + mt.sqrt(disc))/(2*a)
+    except ZeroDivisionError:
+        print("Erro: não é possível dividir por 0")
+        gotor()
+    except ValueError:
+        print("Erro de domínio matemático")
+        pause()
+        exit()
 
-try:
-    float(a), float(b), float(c)
-except ValueError:
-    print("Um ou mais valores dados não foram identificados")
-    pause()
-    exit()
+    print("\nx' = ", res1)
+    print("x'' = ", res2)
+    gotor()
 
-disc = (b**2 - 4*a*c)
-try:
-    res1 = (-b - mt.sqrt(disc))/(2*a)
-    res2 = (-b + mt.sqrt(disc))/(2*a)
-except ZeroDivisionError:
-    print("Erro: não é possível dividir por 0")
-    pause()
-    exit()
-except ValueError:
-    print("Erro de domínio matemático")
-    pause()
-    exit()
-
-print("\nx' = ", res1)
-print("x'' = ", res2)
-
-pause()
+restart()
